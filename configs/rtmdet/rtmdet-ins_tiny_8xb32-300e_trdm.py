e2d1926b903084f84283a972c6981340804b91da
Jonas Stankevičius <jonas_stankevicius@trimble.com>
2024-04-23 10:52:43 +0300

    add more details to trdm config

--- configs/rtmdet/rtmdet-ins_tiny_8xb32-300e_trdm.py
+++ configs/rtmdet/rtmdet-ins_tiny_8xb32-300e_trdm.py
@@ -7,11 +7,38 @@ model = dict(
         init_cfg=dict(
             type='Pretrained', prefix='backbone.', checkpoint=checkpoint)),
     neck=dict(in_channels=[96, 192, 384], out_channels=96, num_csp_blocks=1),
-    bbox_head=dict(in_channels=96, feat_channels=96))
+    bbox_head=dict(
+        type='RTMDetInsSepBNHead',
+        in_channels=96,
+        feat_channels=96,
+        _delete_=True,
+        num_classes=80,
+        stacked_convs=2,
+        share_conv=True,
+        pred_kernel_size=1,
+        act_cfg=dict(type='SiLU', inplace=True),
+        norm_cfg=dict(type='SyncBN', requires_grad=True),
+        anchor_generator=dict(
+            type='MlvlPointGenerator', offset=0, strides=[8, 16, 32]),
+        bbox_coder=dict(type='DistancePointBBoxCoder'),
+        loss_cls=dict(
+            type='QualityFocalLoss',
+            use_sigmoid=True,
+            beta=2.0,
+            loss_weight=1.0),
+        loss_bbox=dict(type='GIoULoss', loss_weight=2.0),
+        loss_mask=dict(
+            type='DiceLoss', loss_weight=2.0, eps=5e-6, reduction='mean')),
+    test_cfg=dict(
+        nms_pre=1000,
+        min_bbox_size=0,
+        score_thr=0.05,
+        nms=dict(type='nms', iou_threshold=0.6),
+        max_per_img=100,
+        mask_thr_binary=0.5),
+)
 
 train_pipeline = [
-    # data/coco/annotations/instances_train2017.json
-    # /mnt/e/coco/annotations/instances_val2017.json
     dict(type='LoadImageFromFile', backend_args={{_base_.backend_args}}),
     dict(
         type='LoadAnnotations',
@@ -45,12 +72,16 @@ train_pipeline = [
     dict(type='PackDetInputs')
 ]
 
+batch_size = 4
+num_workers = 10
+data_root = '/mnt/e/[TRM][026][001]Road-annotation_2024-01-24_07-39-44'
+
 train_dataloader = dict(
-    batch_size=16,
-    num_workers=10,
+    batch_size=batch_size,
+    num_workers=num_workers,
     dataset=dict(
         type='TrdmDataset',
-        data_root='/mnt/e/[TRM][026][001]Road-annotation_2024-01-24_07-39-44',
+        data_root=data_root,
         ann_file='train.json',
         data_prefix=dict(img=''),
         filter_cfg=dict(filter_empty_gt=True, min_size=32),
@@ -70,11 +101,11 @@ test_pipeline = [
 ]
 
 val_dataloader = dict(
-    batch_size=8, 
-    num_workers=10, 
+    batch_size=batch_size, 
+    num_workers=num_workers, 
     dataset=dict(
         type='TrdmDataset',
-        data_root='/mnt/e/[TRM][026][001]Road-annotation_2024-01-24_07-39-44',
+        data_root=data_root,
         ann_file='val.json',
         data_prefix=dict(img=''),
         filter_cfg=dict(filter_empty_gt=True, min_size=32),
@@ -82,3 +113,13 @@ val_dataloader = dict(
         backend_args=None)
 )
 test_dataloader = val_dataloader
+
+val_evaluator = dict(
+    type='CocoMetric',
+    ann_file=data_root+'/val.json',
+    metric=['bbox', 'segm'],
+    backend_args=None)
+test_evaluator = val_evaluator
+
+# val_evaluator = dict(type='SemSegMetric', iou_metrics=['mIoU'])
+# test_evaluator = val_evaluator
