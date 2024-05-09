6cfdd4123524ca65280af53ddc792091180b5bf4
Jonas Stankevičius <jonas_stankevicius@trimble.com>
2024-05-09 15:42:26 +0300

    Adjust config for optimal accuracy

--- configs/rtmdet/rtmdet-ins_tiny_8xb32-300e_trdm.py
+++ configs/rtmdet/rtmdet-ins_tiny_8xb32-300e_trdm.py
@@ -1,25 +1,50 @@
 _base_ = './rtmdet-ins_s_8xb32-300e_coco.py'
 checkpoint = 'https://download.openmmlab.com/mmdetection/v3.0/rtmdet/cspnext_rsb_pretrain/cspnext-tiny_imagenet_600e.pth'  # noqa
 model = dict(
+    type='RTMDet',
+    data_preprocessor=dict(
+        type='DetDataPreprocessor',
+        mean=[127.5, 127.5, 127.5],
+        std=[255, 255, 255],
+        bgr_to_rgb=False,
+        batch_augments=None),
     backbone=dict(
+        type='CSPNeXt',
+        arch='P5',
+        expand_ratio=0.5,
         deepen_factor=0.167,
         widen_factor=0.375,
+        out_indices=(1,2,3,4),
+        channel_attention=True,
+        norm_cfg=dict(type='SyncBN'),
+        act_cfg=dict(type='SiLU', inplace=True),
         init_cfg=dict(
             type='Pretrained', prefix='backbone.', checkpoint=checkpoint)),
-    neck=dict(in_channels=[96, 192, 384], out_channels=96, num_csp_blocks=1),
+    neck=dict(
+        type='CSPNeXtPAFPN',
+        in_channels=[48, 96, 192, 384],
+        out_channels=96,
+        num_csp_blocks=1,
+        expand_ratio=0.5,
+        norm_cfg=dict(type='SyncBN'),
+        act_cfg=dict(type='SiLU', inplace=True)),    
     bbox_head=dict(
         type='RTMDetInsSepBNHead',
         in_channels=96,
         feat_channels=96,
         _delete_=True,
-        num_classes=80,
+        num_classes=3,
         stacked_convs=2,
         share_conv=True,
         pred_kernel_size=1,
+        mask_loss_stride=2,
         act_cfg=dict(type='SiLU', inplace=True),
         norm_cfg=dict(type='SyncBN', requires_grad=True),
         anchor_generator=dict(
-            type='MlvlPointGenerator', offset=0, strides=[8, 16, 32]),
+            type='MlvlPointGenerator', 
+            offset=0, 
+            strides=[4, 8, 16, 32]
+        ),
         bbox_coder=dict(type='DistancePointBBoxCoder'),
         loss_cls=dict(
             type='QualityFocalLoss',
@@ -29,6 +54,11 @@ model = dict(
         loss_bbox=dict(type='GIoULoss', loss_weight=2.0),
         loss_mask=dict(
             type='DiceLoss', loss_weight=2.0, eps=5e-6, reduction='mean')),
+    train_cfg=dict(
+        assigner=dict(type='DynamicSoftLabelAssigner', topk=13),
+        allowed_border=-1,
+        pos_weight=-1,
+        debug=False),
     test_cfg=dict(
         nms_pre=1000,
         min_bbox_size=0,
@@ -38,7 +68,7 @@ model = dict(
         mask_thr_binary=0.5),
 )
 
-input_image_size = 640
+input_image_size = 1280
 
 train_pipeline = [
     dict(type='LoadImageFromFile', backend_args={{_base_.backend_args}}),
@@ -55,7 +85,7 @@ train_pipeline = [
 ]
 
 batch_size = 4
-num_workers = 10
+num_workers = 1
 data_root = '/mnt/e/[TRM][026][001]Road-annotation_2024-01-24_07-39-44'
 
 train_dataloader = dict(
@@ -97,7 +127,7 @@ test_dataloader = val_dataloader
 
 train_cfg = dict(
     max_epochs=50,
-    val_interval=1)
+    val_interval=50)
 
 val_evaluator = dict(
     type='CocoMetric',
